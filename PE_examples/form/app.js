b4c34ad45a3169f24e2b148afb9bb630e68e4f97
Ana Sanz <35330393+Ahisanz@users.noreply.github.com>
2020-11-09 17:22:46 +0000

    New basic Form with aria-checked and better accessibility

--- PE_examples/form/app.js
+++ PE_examples/form/app.js
@@ -1,23 +1,25 @@
-// var form = document.querySelector(".form-js");
-// var log = document.querySelector("#log");
-
-// form.addEventListener("submit", function(event) {
-//   var data = new FormData(form);
-//   var output = "";
-//   for (const entry of data) {
-//     output = output + entry[0] + "=" + entry[1] + "\r";
-//   };
-//   log.innerText = output;
-//   event.preventDefault();
-// }, false);
+// listen to click in the form and change rating score
 
 var formJs = document.querySelector(".form-js");
 
 formJs.addEventListener("click", function (event) {
   if (event.target.name === "rating-form__js") {
-    console.log(event.target.value);
     var value = event.target.value;
 
     // mandar para a API o value
+  }
+});
+
+// Change state of the aria-checked if clicked, focused
+
+var formA11y = document.querySelector(".form-a11y .form__form");
+var formA11yInputs = document.querySelectorAll(".form-a11y .form__form input");
+
+formA11y.addEventListener("click", function (event) {
+  for (i = 0; i < formA11yInputs.length; i++) {
+    formA11yInputs[i].setAttribute("aria-checked", false);
+  }
+  if (event.target.name == "rating-form__a11y") {
+    event.target.setAttribute("aria-checked", true);
   }
 });
